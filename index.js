c1c1113f40296330be502c01de54962b70811fd6
Kovacs Erik <kovaroberik@gmail.com>
2020-01-28 16:46:31 +0200

    needs debug

--- index.js
+++ index.js
@@ -22,6 +22,32 @@ function RSA() {
     }
   };
 
+  const _gcd = (a, b) => {
+    // Everything divides 0
+    if (a == 0) return b;
+    if (b == 0) return a;
+
+    // base case
+    if (a == b) return a;
+
+    // a is greater
+    if (a > b) {
+      return _gcd(a - b, b);
+    } else {
+      return _gcd(a, b - a);
+    }
+  };
+
+  const _areCoprime = (a, b) => _gcd(a, b) === 1;
+
+  const _getE = (n, phi) => {
+    for (let e = 3; e < phi; e += 2) {
+      if (_areCoprime(e, phi) && _areCoprime(e, n)) {
+        return e;
+      }
+    }
+  };
+
   this.getKeys = () => {
     if (!_pub && !_priv) {
       throw new Error(
@@ -34,41 +60,37 @@ function RSA() {
   this.generateKeys = (p, q) => {
     const n = p * q;
     const phi = _phi(p, q);
-    const e = 3;
+    const e = _getE(n, phi);
     const d = _modinv(e, phi);
     _pub = [e, n];
     _priv = [d, n];
   };
 
   this.encrypt = (plain, key) => {
-    plain = Buffer.from(plain).toString("base64");
+    //plain = Buffer.from(plain).toString("base64");
     plain = plain.split("");
     const cypher = plain.map((char, index) => {
       const number = char.charCodeAt(0);
-      console.log("num1=", number);
       const enc = Math.pow(number, key[0]) % key[1];
-      console.log("enc=", enc);
-      return enc.toString();
+      return enc;
     });
     return cypher;
   };
   this.decrypt = (cypher, key) => {
     let plain = cypher.map((hex, index) => {
-      const number = parseInt(hex);
-      console.log("num2=", number);
+      const number = hex;
       const dec = Math.pow(number, key[0]) % key[1];
-      console.log("dec=", dec);
       return String.fromCharCode(dec);
     });
-    return Buffer.from(plain.join("")).toString("utf8");
+    return plain.join(""); //Buffer.from(plain.join("")).toString("utf8");
   };
 }
 
 const rsa = new RSA();
-rsa.generateKeys(5, 11);
+rsa.generateKeys(2, 7);
 const keys = rsa.getKeys();
-const encrypted = rsa.encrypt("Test text", keys.pub);
+const encrypted = rsa.encrypt("Test", keys.pub);
 const decrypted = rsa.decrypt(encrypted, keys.priv);
 console.log(keys);
 console.log(encrypted);
-console.log(decrypted);
+console.log(`[${decrypted}]`);
